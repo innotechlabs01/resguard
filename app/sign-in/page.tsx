770bbed27c2cfabf15283812cb3863211629d64b
Anthony Rivera <anthonyrivera51@gmail.com>
2026-03-27 07:47:54 -0500

    Fix error

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -2,6 +2,8 @@
 
 import { ClerkSignInShell } from '@/components/auth/clerk-sign-in-shell'
 
+export const dynamic = 'force-dynamic'
+
 export default function SignInPage() {
   return <ClerkSignInShell />
 }
